01080ab6ee0f733c5ebb2e5558183909ae9e9d48
ristachristynainggolan-cpu <ristachristynainggolan@gmail.com>
2026-04-13 10:59:14 +0700

    Menambahkan halaman home dan about

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home');
+Route::view('/home', 'home');
+Route::view('/about', 'about');
