fcb5ca4ce5bd012f63ae182c2f81f688896815a1
stanleyandre19 <stanleyandre19@gmail.com>
2026-04-13 12:31:10 +0700

    menambahkan product dan contact blade

--- tugas4/routes/web.php
+++ tugas4/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BarangController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Tambahan route kamu
+Route::get('/barang', [BarangController::class, 'tampilkan']);
