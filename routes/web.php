0c1c8994d533b7365337042de6cb34234e697a2c
ristachristynainggolan-cpu <ristachristynainggolan@gmail.com>
2026-04-13 11:07:12 +0700

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
-Route::view('/home', 'home');
-Route::view('/about', 'about');
+/*
+|--------------------------------------------------------------------------
+| Web Routes - Sistem Rapot Siswa
+|--------------------------------------------------------------------------
+*/
+
+Route::view('/', 'home');        // Halaman utama
+Route::view('/home', 'home');    // Home
+Route::view('/about', 'about');  // Tentang sistem
